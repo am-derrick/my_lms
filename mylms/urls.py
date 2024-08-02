a57fce68e5233571568c59abde196bfae2992c08
Derrick Ampire <ampire90@gmail.com>
2024-08-02 15:20:01 +0300

    role parameter added

--- mylms/urls.py
+++ mylms/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
 	path('accounts/', include('allauth.urls')),
 	path('api/auth/', include('dj_rest_auth.urls')),
 	path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
-	path('signup/', signup, name='signup'),
+	path('signup/<str:role>/', signup, name='signup'),
 ]
